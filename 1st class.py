d58110825d1707b86724f65c9b6822195d8180c6
samirawkhan <90940235+samirawkhan@users.noreply.github.com>
2021-09-19 17:11:25 -0400

    Add files via upload

---
+++ 1st class.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a = 3
+b = 4
+c = 22
+d = 44
+
+print("The value of (a+5)b is ", (a+5)*b)
+
+print("The value of ab/cd is", (a*b)/(c*d))
+
+print("The value of ab/c *d is ", ((a*b)/c)*d)
+
+print("The value of (a+b)(c+d) is ", (a+b)*(c+d))
+
+print("The value of ab^3 is ", a*b**3)
+
+print("The value of (ab)^3 is ", (a*b)**3)
